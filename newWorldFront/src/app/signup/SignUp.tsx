87f25b3d42ec3ad664c64a543bff0becf2ecce6d
catmaker <rzt0317@nate.com>
2024-01-11 04:26:34 +0900

    회원가입 페이지 스타일 및 레이아웃 잡기

--- newWorldFront/src/app/signup/SignUp.tsx
+++ newWorldFront/src/app/signup/SignUp.tsx
@@ -8,24 +8,66 @@ const SignUp = () => {
   return (
     <div className={styles.container}>
       <div className={styles.flexbox}>
-        <div></div>
         <div className={styles.login_box}>
-          <p>START FOR FREE</p>
-          <h1>Create new account</h1>
-          <p>
-            Already A Member? <Link href={`/login`}>Log In</Link>
-          </p>
-          <form action="">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-            <input type="text" placeholder="Confirm Password" />
-            <input type="submit" value="Sign Up" />
-          </form>
+          <header className={styles.header}>
+            <div className={styles.img_box}>
+              <img src="/img/logo2.png" alt="" />
+            </div>
+            <div className={styles.flex}>
+              <h1>New World</h1>
+              <ul className={styles.flex}>
+                <li>Home</li>
+                <li>Main</li>
+              </ul>
+            </div>
+          </header>
+          <div className={styles.content}>
+            <div>
+              <p className={styles.content_p}>START FOR FREE</p>
+              <h1>Create new account</h1>
+              <span>
+                Already A Member? <Link href={`/login`}>Log In</Link>
+              </span>
+            </div>
+            <form action="">
+              <div className={styles.box_size}>
+                <input
+                  className={styles.content_input_name_box}
+                  type="text"
+                  placeholder="First Name"
+                />
+                <input
+                  className={styles.content_input_name_box}
+                  type="text"
+                  placeholder="Last Name"
+                />
+              </div>
+              <div>
+                <input
+                  className={styles.content_input_box}
+                  type="text"
+                  placeholder="Email"
+                />
+                <input
+                  className={styles.content_input_box}
+                  type="text"
+                  placeholder="Password"
+                />
+                <input
+                  className={styles.content_input_box}
+                  type="text"
+                  placeholder="Confirm Password"
+                />
+              </div>
+              <input
+                className={styles.signUp_button}
+                type="submit"
+                value="Sign Up"
+              />
+            </form>
+          </div>
         </div>
       </div>
-      );
     </div>
   );
 };
