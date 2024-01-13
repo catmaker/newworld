3136b8cb9017dc1096bb62da9a9528f37b7278a4
catmaker <rzt0317@nate.com>
2024-01-14 05:11:30 +0900

    fix : 안쓰는 코드 제거

--- newWorldFront/src/app/intro/Intro.tsx
+++ newWorldFront/src/app/intro/Intro.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
 import styles from "./intro.module.scss";
-import { titles } from "./dummy";
 import Playbutton from "./Playbutton";
 import Link from "next/link";
 import { Link as ScrollLink, Element, scroller } from "react-scroll";
