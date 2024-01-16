4f6f88fd8a31029ed17d5e3f88c84bc9b5fdea6e
catmaker <rzt0317@nate.com>
2024-01-16 18:36:45 +0900

    feat : 비 내리는 효과 추가

--- newWorldFront/src/app/labyrinth/Labyrinth.tsx
+++ newWorldFront/src/app/labyrinth/Labyrinth.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "./labyrinth.module.scss";
 
-const labyrinth = () => {
-  return <div>labyrinth</div>;
+const Labyrinth = () => {
+  const stars = Array.from({ length: 100 }).map((_, i) => {
+    const length = Math.random() * 5;
+    return (
+      <div
+        key={i}
+        className={styles.star}
+        style={{
+          animationDuration: `${length}s`,
+          left: `${Math.random() * 100}vw`,
+          top: "-10vh",
+          animationDelay: `${Math.random() * 5}s`,
+          transform: "rotate(45deg)",
+          height: `${length}vh`,
+        }}
+      />
+    );
+  });
+
+  return <div className={styles.container}>{stars}</div>;
 };
 
-export default labyrinth;
+export default Labyrinth;
