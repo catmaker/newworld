e6f797f9967b9ec1b6f16ee74f78a9ce6a6bccbf
catmaker <rzt0317@nate.com>
2024-01-16 17:19:07 +0900

    fix : 빌드 오류 수정 @ src 지정

--- newWorldFront/next.config.js
+++ newWorldFront/next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
