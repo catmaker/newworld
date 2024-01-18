229707892181384469fa7e334b00d7ba474ac506
jeonbyeong-il <ekdh523@gmail.com>
2024-01-19 00:08:18 +0900

    연결test

--- newWorldBack/src/main/java/NewWorld/test/testController.java
+++ newWorldBack/src/main/java/NewWorld/test/testController.java
@@ -4,14 +4,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("/")
+@RestController
 public class testController {
 
-    @GetMapping("test")
+    @GetMapping("/test")
+    @ResponseBody
     public String test(HttpServletRequest request) {
-
     return "성공";
     }
 }
