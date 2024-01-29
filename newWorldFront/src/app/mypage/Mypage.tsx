729df8018ab09a18a92777a701cc419c50da07a3
catmaker <rzt0317@nate.com>
2024-01-30 04:39:15 +0900

    feat : 더미데이터 활용 point, 해결 문제 수 등 상단 div 컨텐츠 추가

--- newWorldFront/src/app/mypage/Mypage.tsx
+++ newWorldFront/src/app/mypage/Mypage.tsx
@@ -1,9 +1,25 @@
+"use client";
 import React from "react";
 import styles from "./mypage.module.scss";
 import dummy from "./dummy.json";
 import dummy2 from "./dummy2.json";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+
 const Mypage = () => {
+  const [days, setDays] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const signUp = new Date(dummy2.users[0].signUpDate);
+    const current = new Date("2024-01-30");
+
+    const differenceInTime = current.getTime() - signUp.getTime();
+    const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+
+    setDays(Math.ceil(differenceInDays));
+    setLoading(false);
+  }, []);
   return (
     <div className={styles.background}>
       <div className={styles.container}>
@@ -77,9 +93,40 @@ const Mypage = () => {
             </div>
           </div>
           <div className={styles.right_layout}>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={styles.welcome_message}>
+              <div className={styles.message}>
+                {loading ? (
+                  <div className={styles.loading}>
+                    로딩중입니다. 잠시만 기다려주세요.
+                  </div>
+                ) : (
+                  <div>
+                    <p>
+                      환영합니다 <span>{dummy2.users[0].name}</span> 님!
+                    </p>
+                    <p>
+                      저희가 처음 만난 날은 {dummy2.users[0].signUpDate} 입니다.
+                    </p>
+                    <p>처음 만난 날부터 지금까지 {days}일이 지났습니다.</p>
+                  </div>
+                )}
+                <Image
+                  src={"/img/medal/medal1.svg"}
+                  width={20}
+                  height={20}
+                  alt="image"
+                  title="1000 days"
+                ></Image>
+              </div>
+              <div className={styles.point_section}>
+                <div>
+                  <p>해결한 문제 수 : {dummy2.users[0].clear}</p>
+                  <p>획득한 포인트 : {dummy2.users[0].point}</p>
+                </div>
+              </div>
+            </div>
+            <div>123</div>
+            <div>123</div>
           </div>
         </div>
       </div>
