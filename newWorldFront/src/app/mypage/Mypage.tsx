e6d3bbe2953457f9a03e9934a1ccba0af95cdaae
catmaker <rzt0317@nate.com>
2024-01-30 05:13:35 +0900

    feat : 메뉴 추가

--- newWorldFront/src/app/mypage/Mypage.tsx
+++ newWorldFront/src/app/mypage/Mypage.tsx
@@ -9,7 +9,7 @@ import { useState, useEffect } from "react";
 const Mypage = () => {
   const [days, setDays] = useState(0);
   const [loading, setLoading] = useState(true);
-
+  const [selectedItem, setSelectedItem] = useState("");
   useEffect(() => {
     const signUp = new Date(dummy2.users[0].signUpDate);
     const current = new Date("2024-01-30");
@@ -125,8 +125,41 @@ const Mypage = () => {
                 </div>
               </div>
             </div>
-            <div>123</div>
-            <div>123</div>
+            <div className={styles.privacy_control_box}>
+              <div
+                className={`${styles.privacy_control_box_item} ${
+                  selectedItem === "개인정보 관리" ? styles.selected : ""
+                }`}
+                onClick={() => setSelectedItem("개인정보 관리")}
+              >
+                개인정보 관리
+              </div>
+              <div
+                className={`${styles.privacy_control_box_item} ${
+                  selectedItem === "프로필 관리" ? styles.selected : ""
+                }`}
+                onClick={() => setSelectedItem("프로필 관리")}
+              >
+                프로필 관리
+              </div>
+              <div
+                className={`${styles.privacy_control_box_item} ${
+                  selectedItem === "클리어 퀴즈" ? styles.selected : ""
+                }`}
+                onClick={() => setSelectedItem("클리어 퀴즈")}
+              >
+                클리어 퀴즈
+              </div>
+              <div
+                className={`${styles.privacy_control_box_item} ${
+                  selectedItem === "뱃지" ? styles.selected : ""
+                }`}
+                onClick={() => setSelectedItem("뱃지")}
+              >
+                뱃지
+              </div>
+            </div>
+            <div className={styles.infomation}>{selectedItem}</div>
           </div>
         </div>
       </div>
