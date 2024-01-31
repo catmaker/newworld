c50696082bfd912a092b5ba1111ad69ea2d67726
catmaker <rzt0317@nate.com>
2024-02-01 02:22:15 +0900

    feat : post 기능 작성

--- newWorldFront/src/app/post/Post.tsx
+++ newWorldFront/src/app/post/Post.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
 import styles from "./post.module.scss";
+import "react-quill/dist/quill.snow.css";
+import ReactEditor from "@/components/reactQuill/ReactQuill";
 const Post = () => {
-  return <div className={styles.background}>Post</div>;
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <div className={styles.contents_section}>
+          <ReactEditor />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
