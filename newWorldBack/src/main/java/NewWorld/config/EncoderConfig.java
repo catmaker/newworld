30e37c54c23b9340dc06483b730c7e5a09ac0970
jeonbyeong-il <ekdh523@gmail.com>
2024-02-12 16:06:15 +0900

    시큐리티 제거

--- newWorldBack/src/main/java/NewWorld/config/EncoderConfig.java
+++ newWorldBack/src/main/java/NewWorld/config/EncoderConfig.java
@@ -2,13 +2,12 @@ package NewWorld.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 public class EncoderConfig{
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+    //@Bean
+   // public PasswordEncoder passwordEncoder(){
+        //return new BCryptPasswordEncoder();
+   // }
 }
