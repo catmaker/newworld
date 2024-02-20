12bb59482f215d4c7017af52c9b05e321a50be61
catmaker <rzt0317@nate.com>
2024-02-21 07:27:14 +0900

    fix : 주소 변경

--- newWorldFront/src/app/(root)/ranking/Ranking.tsx
+++ newWorldFront/src/app/(root)/ranking/Ranking.tsx
@@ -4,11 +4,31 @@ import styles from "./ranking.module.scss";
 import Link from "next/link";
 import Header from "@/app/components/header/page";
 import { RankingProps } from "@/app/types/Ranking";
-import useCategory from "../community/UseCategory";
+import useCategory from "../../hooks/UseCategory";
 const Ranking: React.FC<RankingProps> = ({ data }) => {
   const { selectedCategory, handleCategoryClick } = useCategory(
     "종합",
     () => {}
+  );
+  const pointKey =
+    selectedCategory === "종합"
+      ? "totalPoints"
+      : selectedCategory === "문제"
+      ? "clearPoints"
+      : "attendancePoints";
+  const sortedByCategory = Object.entries(data).sort(
+    ([, aPoints], [, bPoints]) => {
+      switch (selectedCategory) {
+        case "종합":
+          return bPoints.totalPoints - aPoints.totalPoints;
+        case "문제":
+          return bPoints.clearPoints - aPoints.clearPoints;
+        case "출석":
+          return bPoints.attendancePoints - aPoints.attendancePoints;
+        default:
+          return 0;
+      }
+    }
   );
   return (
     <div className={styles.background}>
@@ -21,7 +41,17 @@ const Ranking: React.FC<RankingProps> = ({ data }) => {
           <li onClick={() => handleCategoryClick("출석")}>출석</li>
         </ul>
       </div>
-      <div>현재 선택된 카테고리: {selectedCategory}</div>
+      <div className={styles.contents_wrapper}>
+        <div className={styles.contents_Layout}>
+          {sortedByCategory.map(([name, points], index) => (
+            <div className={styles.contents_Layout_item} key={name}>
+              <div>Rank: {index + 1}</div>
+              <div>{name}</div>
+              <div>Points: {points[pointKey]}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
