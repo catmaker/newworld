edd001b343cf54f49413d3ab90f21ed5b88d84ef
catmaker <rzt0317@nate.com>
2024-02-21 07:28:02 +0900

    feat: 예상 데이터 추가 데이터 point 순서대로 내림차순 정렬 기능 추가

--- newWorldFront/src/app/api/getRanking/route.ts
+++ newWorldFront/src/app/api/getRanking/route.ts
@@ -18,6 +18,21 @@ export const GET = async (req: NextRequest) => {
         attendancePoints: 100,
         totalPoints: 200,
       },
+      lee: {
+        clearPoints: 200,
+        attendancePoints: 200,
+        totalPoints: 400,
+      },
+      park: {
+        clearPoints: 50,
+        attendancePoints: 100,
+        totalPoints: 150,
+      },
+      choi: {
+        clearPoints: 100,
+        attendancePoints: 100,
+        totalPoints: 200,
+      },
     });
   } catch (error) {
     console.error(error);
