1ff506f9cdaae1d416879996a4b9d5478c020137
catmaker <rzt0317@nate.com>
2024-03-01 23:16:10 +0900

    feat : 퀴즈 전체 불러오기 while문 사용

--- newWorldFront/src/app/quizzes/page.tsx
+++ newWorldFront/src/app/quizzes/page.tsx
@@ -3,8 +3,20 @@ import Quizzes from "./Quizzes";
 import Header from "@/components/header/page";
 import { getQuizzesAPI } from "../lib/api/quizzes";
 const page = async () => {
-  const data = await getQuizzesAPI();
-  console.log(data);
+  let data: any = [];
+  let page = 0;
+
+  while (true) {
+    try {
+      const res = await getQuizzesAPI(page);
+      data = data.concat(res.content);
+      if (res.last) break; // 마지막 페이지에 도달하면 종료
+      page++;
+    } catch (error) {
+      console.error(error);
+      break;
+    }
+  }
   return (
     <>
       <Header />
