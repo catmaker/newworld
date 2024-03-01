51034a58147aae389b0678439e802a3e3aa80021
catmaker <rzt0317@nate.com>
2024-03-02 00:49:46 +0900

    feat : 퀴즈 데이터 받아오기

--- newWorldFront/src/app/quizzes/[id]/Quiz.tsx
+++ newWorldFront/src/app/quizzes/[id]/Quiz.tsx
@@ -1,10 +1,20 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
-import { usePathname } from "next/navigation";
-const Quiz = async () => {
-  useEffect(() => {}, []);
-  return <div>Quiz</div>;
+import React, { useEffect, useState } from "react";
+import { getQuizzesAPI } from "@/app/lib/api/quizzes";
+
+const Quiz = ({ quiz }: any) => {
+  if (!quiz || !Array.isArray(quiz.hints)) {
+    console.error("quiz or quiz.hints is not an array:", quiz);
+    return null;
+  }
+  console.log(quiz);
+  return (
+    <div style={{ color: "white" }}>
+      <h1>{quiz.quizTitle}</h1>
+      <p>{quiz.quizDetail}</p>
+      <p>Answer: {quiz.answer}</p>
+    </div>
+  );
 };
 
 export default Quiz;
--- newWorldFront/src/app/quizzes/[id]/page.tsx
+++ newWorldFront/src/app/quizzes/[id]/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Quiz from "./Quiz";
-const page = (props: any) => {
-  console.log(props.params);
+import { getQuizAPI } from "@/app/lib/api/quizzes";
+const page = async (props: any) => {
   const { id } = props.params;
-  return <Quiz></Quiz>;
+  const quiz = await getQuizAPI(id);
+
+  return <Quiz quiz={quiz}></Quiz>;
 };
 
 export default page;
