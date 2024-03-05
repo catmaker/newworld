6b2fbb19d5ae9c3e046a205323b573822dc0b3e0
catmaker <rzt0317@nate.com>
2024-03-05 16:55:37 +0900

    fix : get에서 post로 바뀐 형식 객체로 전달

--- newWorldFront/src/app/quizzes/[id]/page.tsx
+++ newWorldFront/src/app/quizzes/[id]/page.tsx
@@ -3,8 +3,8 @@ import Quiz from "./Quiz";
 import { getQuizAPI } from "@/app/lib/api/quizzes";
 const page = async (props: any) => {
   const { id } = props.params;
-  const quiz = await getQuizAPI(id);
-  console.log(quiz);
+  const quizId = id;
+  const quiz = await getQuizAPI({ quizId });
   return <Quiz quiz={quiz}></Quiz>;
 };
 
