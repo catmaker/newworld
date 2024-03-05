f48fbb8d08439a72ecc4eda88517dea313e570b1
catmaker <rzt0317@nate.com>
2024-03-05 16:55:16 +0900

    feat : makeQuiz api 생성

--- newWorldFront/src/app/lib/api/quizzes.ts
+++ newWorldFront/src/app/lib/api/quizzes.ts
@@ -21,13 +21,22 @@ export const postCheckQuiz = async (data: any) => {
     console.error(error);
   }
 };
-export const getQuizAPI = async (page: number) => {
+export const getQuizAPI = async (data: any) => {
   try {
     // 퍼즐 제목 / 작성일 / 난이도
-    const response = await axios.get(
-      `http://localhost:8080/getQuiz?quizId=${page}`
-    );
+    const response = await axios.post("http://localhost:8080/getQuiz", data);
     return response.data;
+  } catch (error) {
+    // console.error(error);
+  }
+};
+
+export const postMakeQuiz = async (data: any) => {
+  try {
+    // 보낼 데이터는 퍼즐 제목, 퍼즐 내용, 힌트, 정답, 난이도
+    const response = await axios.post("http://localhost:8080/makeQuiz", data);
+
+    return response;
   } catch (error) {
     console.error(error);
   }
