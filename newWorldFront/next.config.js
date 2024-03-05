f1da1329b43f92f5fb81b39bfd8c15972d2370e9
catmaker <rzt0317@nate.com>
2024-03-05 16:56:21 +0900

    fix : 안쓰는 코드 제거

--- newWorldFront/next.config.js
+++ newWorldFront/next.config.js
@@ -2,9 +2,6 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
