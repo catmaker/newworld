183c6f06cae59c7b30b1248f0abcee683c3a744d
catmaker <rzt0317@nate.com>
2024-03-09 17:45:07 +0900

    fix : 커뮤니티 디자인 수정 랭킹 페이지 데이터 수정(아직 백엔드 수정중)

--- newWorldFront/src/app/lib/api/ranking.ts
+++ newWorldFront/src/app/lib/api/ranking.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 export const getRankingAPI = async () => {
   try {
-    const { data } = await axios.get("http://localhost:8080/getRanking");
-    console.log(data);
-    return data;
+    const data = await axios.get("http://localhost:8080/getTotalRanking");
+    return data.data;
   } catch (error) {
     console.error(error);
-    return {}; // 또는 다른 기본값
   }
 };
