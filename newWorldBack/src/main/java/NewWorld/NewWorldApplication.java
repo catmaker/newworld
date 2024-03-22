c0dbe1d0d394844c35b8dbab918cf44b2ebb1fe3
Jeon_Byeongil <ekdh523@ant-soft.co.kr>
2024-03-22 17:26:31 +0900

    corsfilter

--- newWorldBack/src/main/java/NewWorld/NewWorldApplication.java
+++ newWorldBack/src/main/java/NewWorld/NewWorldApplication.java
@@ -2,6 +2,9 @@ package NewWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NewWorldApplication {
@@ -10,4 +13,13 @@ public class NewWorldApplication {
 		SpringApplication.run(NewWorldApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://newworld-lovat.vercel.app");
+			}
+		};
+	}
 }
